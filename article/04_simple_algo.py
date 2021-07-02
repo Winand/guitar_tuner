588c21ff4a5100e8d1941e0680fae6d95bcb8cf3
Makarov Andrey <winandfx@gmail.com>
2021-07-02 10:32:54 +0300

    Fix: octave names

--- article/04_simple_algo.py
+++ article/04_simple_algo.py
@@ -17,7 +17,7 @@ CONCERT_PITCH = 440
 ALL_NOTES = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
 def find_closest_note(pitch):
   i = int( np.round( np.log2( pitch/CONCERT_PITCH )*12 ) )
-  closestNote = ALL_NOTES[i%12] + str(4 + np.sign(i) * int((9+abs(i))/12) )
+  closestNote = ALL_NOTES[i%12] + str(4 + (i + 9) // 12)
   closestPitch = CONCERT_PITCH*2**(i/12)
   return closestNote, closestPitch
 
